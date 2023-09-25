563f4d21d2bdfbb8065008387ad25553edcde8ba
fiqhinre <fiqhinadya@gmail.com>
2023-09-25 14:45:38 +0700

    PAYMENT DATE

--- database/migrations/2023_09_17_122319_create_payments.php
+++ database/migrations/2023_09_17_122319_create_payments.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->timestamps();
             $table->float('PAYMENT_AMOUNT');
             $table->boolean('PAYMENT_STATUS');
+            $table->dateTime('PAYMENT_DATE');
         });
     }
 
